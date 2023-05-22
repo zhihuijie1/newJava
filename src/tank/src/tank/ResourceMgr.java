12e6a3a16421fb5bf6a9d0d3f6a18642f867e80f
西雅图的雪很美 <92995461+zhihuijie1@users.noreply.github.com>
2023-05-22 22:03:03 +0800

    Add files via upload

---
+++ src/tank/src/tank/ResourceMgr.java
@@ -0,0 +1,33 @@
+package tank;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ResourceMgr {
+    public static BufferedImage tankL;
+    public static BufferedImage tankR;
+    public static BufferedImage tankU;
+    public static BufferedImage tankD;
+
+    public static BufferedImage bulletD;
+    public static BufferedImage bulletU;
+    public static BufferedImage bulletL;
+    public static BufferedImage bulletR;
+
+    static {
+        try {
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
